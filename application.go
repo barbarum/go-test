8f02d3623b27b3deb9c6fc327a34a0a8a877ff68
dwei <dwei@aibee.com>
2020-01-23 15:53:27 +0800

    Move to another folder

--- application.go
+++ application.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/barbarum/go-test/src/main/greeting"
+	// "github.com/google/go-cmp/cmp"
+)
 
 func main() {
 	fmt.Println("Hello, go application developer!")
+	fmt.Println(greeting.Reverse("!oG"))
+	// fmt.Println(cmp.Diff("test", "test1"))
 }
